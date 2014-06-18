1c1b98d6e6cbeed38a9f629f6907655c18d73265
Tomas Flouri <Tomas.Flouri@h-its.org>
2014-06-19 00:27:56 +0200

    modified version number

--- src/pear.h
+++ src/pear.h
@@ -2,8 +2,8 @@
 #define _PEAR_
 
 #define PROGRAM_NAME            "PEAR"
-#define PROGRAM_VERSION         "0.9.2"
-#define VERSION_DATE            "March 26 2014"
+#define PROGRAM_VERSION         "0.9.3"
+#define VERSION_DATE            "June 19 2014"
 #define LICENCE                 "Creative Commons Licence"
 #define CONTACT                 "Tomas.Flouri@h-its.org and Jiajie.Zhang@h-its.org"
 
