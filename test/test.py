1b81ca8e3c8da63abfebb541fc6f6edd36ee5cb6
zhangjiajie <bestzhangjiajie@gmail.com>
2014-08-08 16:28:33 +0200

    update test script

--- test/test.py
+++ test/test.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
                   ]
     i = 1
     for test in test_files:
-        gen(test[0], test[1], "temp", test[2])
-        #flag = unit_test(test[0], test[1], "temp", test[2])
-        #print("Test " + repr(i) + " " + flag)
+        #gen(test[0], test[1], "temp", test[2])
+        flag = unit_test(test[0], test[1], "temp", test[2])
+        print("Test " + repr(i) + " " + flag)
         i = i + 1
